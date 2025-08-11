0f64bf4051a60d84895c1d89a4295f845e76a037
broscoe <bakari.roscoe@gmail.com>
2025-08-11 15:21:18 -0400

    fixed Home.jsx for versions 3,4,5

--- version-4/client/src/pages/Home.jsx
+++ version-4/client/src/pages/Home.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import { For, Stack, Input } from "@chakra-ui/react";
-import { FormControl, FormLabel } from "@chakra-ui/form-control";
-import { Field } from "../components/ui/field";
-import { NativeSelectField, NativeSelectRoot } from "../components/ui/native-select";
 import CountryCard from "../customComponents/CountryCard";
 import { useEffect, useState } from 'react';
 
 export default function Home({ countries }) {
-  const [formData, setFormData] = useState({
-    search: '',
-  });
 
   const [userFilteredCountries, setUserFilteredCountries] = useState(countries);
   
@@ -17,39 +11,10 @@ export default function Home({ countries }) {
     setUserFilteredCountries(countries);
   }, [countries]);
 
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    console.log(name, value);
-    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
-    setUserFilteredCountries(countries.filter(country => {
-      return country.name.official.includes(...formData, search);
-    }))
-  };
-
-  const FormComponent = () => (
-    <FormControl>
-      <FormLabel htmlFor="search">Search</FormLabel>
-      <Field id="search" name="search" placeholder="Search countries" />
-      <FormLabel htmlFor="search">Filter</FormLabel>
-      <NativeSelectRoot>
-        <NativeSelectField id="filter" name="filter">
-          <option value="Africa">Africa</option>
-          <option value="Americas">Americas</option>
-          <option value="Asia">Asia</option>
-          <option value="Europe">Europe</option>
-          <option value="Oceania">Oceania</option>
-        </NativeSelectField>
-      </NativeSelectRoot>
-      <FormLabel htmlFor="search">Search</FormLabel>
-      <Input type="text" id="search" name="search" placeholder="Search" value={formData.search} onChange={handleChange} />
-    </FormControl>
-  );
-
   return <>
 
-    {/* push name from obj saved to array to populate saved countries */}
-    <FormComponent />
+    
+  
 
     <Stack gap="4" direction="row" wrap="wrap">
       <For each={userFilteredCountries} >
